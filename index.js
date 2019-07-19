2b0a3371eec21659b2472714c906e2bf2ff4ee98
Justin <jbbenson12@gmail.com>
2019-07-18 19:29:21 -1000

    email hell

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const emailLog = require('./email_log.json');
+const emailLog = require("./email_log.json");
 
 function countUniqueEmails(emailFile) {
   // do work here
+
+  // [1, 2, 3, 4]
+
+  // reduce(function(p, c){
+  //   return p[c]
+  // }, {})
+
+  // should return {1: , 2: , 3: , 4: }
+  return emailFile.emails.reduce((p, c) => {
+    if (!p[c.email]) {
+      p[c.email] = 1;
+    } else {
+      p[c.email]++;
+    }
+    return p;
+    // p[c] = (p[c] || 0) + 1;
+    // return p[c];
+  }, {});
 }
 
 console.log(countUniqueEmails(emailLog));
